4fd6b90b40ca961be064e38a7b7aaa883c7e7108
Renaud Merle <rmerle@cockpit-itsm.com>
2019-06-20 00:49:42 +0200

    fix: check there is a sourcemap before fixing pathes

--- task.js
+++ task.js
@@ -22,6 +22,7 @@ function saveCoverage (coverage) {
 function fixSourcePathes (coverage) {
   Object.keys(coverage).forEach(file => {
     const sourcemap = coverage[file].inputSourceMap
+    if (!sourcemap) return
     sourcemap.sources = sourcemap.sources.map(source => {
       let cleaned = source
       if (cleaned.includes('!')) cleaned = cleaned.split('!').pop()

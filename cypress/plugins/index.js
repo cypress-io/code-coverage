b8c7a6e51ee30f96203e5ca31123038e275b0c80
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-07-22 22:16:56 -0400

    feat: add non .babelrc method (#48)

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -5,4 +5,8 @@ module.exports = (on, config) => {
   // to get the code coverage from unit tests
   // https://glebbahmutov.com/blog/combined-end-to-end-and-unit-test-coverage/
   on('file:preprocessor', require('../../use-babelrc'))
+
+  // or use browserify and just push babel-plugin-istanbul
+  // directory to the list of babelify plugins
+  // on('file:preprocessor', require('../../use-browserify-istanbul'))
 }

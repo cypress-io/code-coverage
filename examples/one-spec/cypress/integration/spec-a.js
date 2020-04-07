0c646adc4c36f54d77cf26bec3abd27b6382e426
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-07 17:54:31 -0400

    more coverage for one-spec

--- examples/one-spec/cypress/integration/spec-a.js
+++ examples/one-spec/cypress/integration/spec-a.js
@@ -2,4 +2,12 @@
 it('spec a', () => {
   cy.visit('index.html')
   cy.contains('Page body')
+
+  cy.window()
+    .invoke('add', 2, 3)
+    .should('equal', 5)
+
+  cy.window()
+    .invoke('sub', 2, 3)
+    .should('equal', -1)
 })

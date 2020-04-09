9416a81c08b90ece48cf52b268ba71ba86daa800
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-09 17:13:26 -0400

    add screenshot and unit tests

--- examples/fullstack/cypress/integration/spec.js
+++ examples/fullstack/cypress/integration/spec.js
@@ -1,4 +1,8 @@
 /// <reference types="Cypress" />
+
+// load extra files to instrument on the fly
+const { reverse } = require('../../string-utils')
+
 it('uses frontend code and calls backend', () => {
   cy.visit('/')
   cy.contains('Page body').should('be.visible')
@@ -13,4 +17,7 @@ it('uses frontend code and calls backend', () => {
 
   cy.log('**backend request**')
   cy.request('/hello')
+
+  cy.log('**unit test**')
+  expect(reverse('Hello')).to.equal('olleH')
 })

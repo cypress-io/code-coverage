20a2a9a1993ae2380f4f1d314e906c66b0a86924
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-17 14:09:27 -0400

    fix: print number of keys and few paths for simpler debugging

--- task.js
+++ task.js
@@ -44,9 +44,26 @@ function saveCoverage(coverage) {
  */
 function resolvePaths(nycFilename) {
   const nycCoverage = JSON.parse(readFileSync(nycFilename, 'utf8'))
+
+  const coverageKeys = Object.keys(nycCoverage)
+  if (!coverageKeys.length) {
+    console.error('⚠️ file %s has no coverage information', nycFilename)
+    return
+  }
+  debug('NYC file %s has %d key(s)', nycFilename, coverageKeys.length)
+
   let changed
-  Object.keys(nycCoverage).forEach(key => {
+  const maxPrintKeys = 3
+
+  Object.keys(nycCoverage).forEach((key, k) => {
     const coverage = nycCoverage[key]
+
+    // printing a few found keys and file paths from the coverage file
+    // will make debugging any problems much much easier
+    if (k < maxPrintKeys) {
+      debug('%d key %s file path %s', k + 1, key, coverage.path)
+    }
+
     if (coverage.path && !isAbsolute(coverage.path)) {
       if (existsSync(coverage.path)) {
         debug('resolving path %s', coverage.path)

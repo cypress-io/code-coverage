49d0ed1efe30d925d0d35c92723dcd3391a33014
Ely Lucas <ely@meta-tek.net>
2022-01-27 08:37:15 -0700

    fix

--- examples/old-cypress-config/fullstack/cypress/plugins/index.js
+++ examples/old-cypress-config/fullstack/cypress/plugins/index.js
@@ -1,6 +1,4 @@
 module.exports = (on, config) => {
   require('../../../../../task')(on, config)
-  // instrument loaded spec files (and the application code loaded from them)
-  on('file:preprocessor', require('../../../../use-browserify-istanbul'))
   return config
 }

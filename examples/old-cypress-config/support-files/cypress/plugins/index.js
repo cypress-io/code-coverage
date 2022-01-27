b7297540c31ec6d86b5ddab21f5b6ccbff6539c1
Ely Lucas <ely@meta-tek.net>
2022-01-27 15:07:49 -0700

    support files

--- examples/old-cypress-config/support-files/cypress/plugins/index.js
+++ examples/old-cypress-config/support-files/cypress/plugins/index.js
@@ -1,5 +1,5 @@
 module.exports = (on, config) => {
   require('../../../../../task')(on, config)
-  on('file:preprocessor', require('../../../../../use-babelrc'))
+  on('file:preprocessor', require('cypress-istanbul/use-babelrc'))
   return config
 }

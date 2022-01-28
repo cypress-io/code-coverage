c46fed95bb1f5c56f7fcee774cd7a01e1ce57d2d
Ely Lucas <ely@meta-tek.net>
2022-01-28 15:52:27 -0700

    fix

--- examples/old-cypress-config/frontend/app.js
+++ examples/old-cypress-config/frontend/app.js
@@ -2,8 +2,4 @@ import { map } from 'lodash'
 
 const list = [{ name: 'joe' }, { name: 'mary' }]
 const names = map(list, 'name')
-if (true) {
-  console.log('just names', names)
-} else {
-  console.error('never reached')
-}
+console.log('just names', names)

534e38504b278508643d1039d1c032cfbb320656
Ely Lucas <ely@meta-tek.net>
2022-01-28 17:34:58 -0700

    removing cypress/browserify-preprocessor

--- use-babelrc.js
+++
@@ -1,9 +0,0 @@
-const browserify = require('@cypress/browserify-preprocessor')
-
-// TODO check if there is .babelrc file
-// if not, maybe create one?
-
-// Tells Cypress to use .babelrc when bundling spec code
-const options = browserify.defaultOptions
-options.browserifyOptions.transform[1][1].babelrc = true
-module.exports = browserify(options)

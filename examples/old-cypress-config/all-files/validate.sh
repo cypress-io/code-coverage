f25d800364ac162640e8f24c0de13e965ba56de8
Ely Lucas <ely@meta-tek.net>
2022-02-01 04:23:49 -0700

    wip

--- examples/old-cypress-config/all-files/validate.sh
+++
@@ -1,4 +0,0 @@
-../node_modules/.bin/check-coverage main.js
-../node_modules/.bin/check-coverage second.js
-../node_modules/.bin/check-coverage not-covered.js
-../node_modules/.bin/only-covered --from coverage/coverage-final.json main.js second.js not-covered.js

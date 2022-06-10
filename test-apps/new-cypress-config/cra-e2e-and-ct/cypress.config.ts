004cfbc4a2ee567c000f72b3c23d60f71d9ff5a9
Ely Lucas <ely@meta-tek.net>
2022-06-10 16:31:00 -0600

    chore: adding cra test app with e2e and ct testing for cy10 config

---
+++ test-apps/new-cypress-config/cra-e2e-and-ct/cypress.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'cypress'
+import '@cypress/instrument-cra'
+
+export default defineConfig({
+  env: {
+    codeCoverage: {
+      exclude: 'cypress/**/*.*'
+    }
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
+      return config
+    }
+  },
+
+  component: {
+    devServer: {
+      framework: 'create-react-app',
+      bundler: 'webpack'
+    },
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
+      return config
+    }
+  }
+})

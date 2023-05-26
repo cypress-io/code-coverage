bee4f56d47c25a5b29f479091e0a455ebac209b3
Frederik Claus <f.v.claus@googlemail.com>
2023-05-26 13:23:09 +0200

    fix: Prettier and code coverage

--- support.js
+++ support.js
@@ -75,14 +75,12 @@ const registerHooks = () => {
       const existingCoverage = Cypress._.find(windowCoverageObjects, {
         coverage: applicationSourceCoverage
       })
-      if (
-        existingCoverage
-      ) {
+      if (existingCoverage) {
         // this application code coverage object is already known
         // which can happen when combining `window:load` and `before` callbacks
         // it can also happen when the user leaves and returns to the application under test
         // in which case we need to use new applicationSourceCoverage, because the old will not be updated anymore.
-        existingCoverage.coverage = applicationSourceCoverage;
+        existingCoverage.coverage = applicationSourceCoverage
         return
       }
 

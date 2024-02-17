cabd30fcc01f049042e69f8e253f798bcb171b1c
Rohit Gohri <code@rohit.page>
2024-02-17 23:24:09 +0100

    koa

---
+++ middleware/koa.js
@@ -0,0 +1,37 @@
+// @ts-check
+const { isCoverageEnabled } = require('../lib/common/isEnabled')
+
+/**
+ * for Express.js
+ *
+ * @example Use like
+ * ```ts
+ * const Koa = require('koa')
+ * const app = new Koa();
+ * // @see https://github.com/rohit-gohri/cypress-code-coverage-v8
+ * require('cypress-code-coverage-v8/middleware/koa')(app)
+ * ```
+ *
+ * @param {import('koa')} app
+ */
+module.exports = (app) => {
+  // expose "GET __coverage__" endpoint that just returns
+  // global coverage information (if the application has been instrumented)
+  app.use(async (ctx, next) => {
+    if (ctx.path !== '/__coverage__' && ctx.method !== 'GET') {
+      return next()
+    }
+
+    if (!isCoverageEnabled()) {
+      ctx.body = {
+        coverage: null
+      }
+      return
+    }
+
+    const { getCoverage } = require('../lib/register/getCoverage')
+    ctx.body = {
+      coverage: (await getCoverage()) || null
+    }
+  })
+}
